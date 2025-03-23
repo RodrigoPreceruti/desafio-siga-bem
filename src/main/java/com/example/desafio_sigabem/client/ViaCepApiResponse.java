77e85774dc1cc334eebedbd312e3ab247d59459f
Rodrigo <rodrigoprecerutti28@gmail.com>
2025-03-23 19:01:12 -0300

    feat: integracao viacep

---
+++ src/main/java/com/example/desafio_sigabem/client/ViaCepApiResponse.java
@@ -0,0 +1,18 @@
+package com.example.desafio_sigabem.client;
+
+public record ViaCepApiResponse(
+        String cep,
+        String logradouro,
+        String complemento,
+        String unidade,
+        String bairro,
+        String localidade,
+        String uf,
+        String estado,
+        String regiao,
+        String ibge,
+        String gia,
+        String ddd,
+        String siafi
+) {
+}
---
+++ src/main/java/com/example/desafio_sigabem/client/ViaCepClient.java
@@ -0,0 +1,11 @@
+package com.example.desafio_sigabem.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "via-cep", url = "https://viacep.com.br/ws")
+public interface ViaCepClient {
+    @GetMapping("/{cep}/json/")
+    ViaCepApiResponse buscarCep(@PathVariable String cep);
+}
