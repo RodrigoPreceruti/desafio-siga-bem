41cc90ccd8ace67615e670939a6728f20900cc8b
Rodrigo <rodrigoprecerutti28@gmail.com>
2025-03-23 13:00:40 -0300

    first commit

---
+++ src/main/java/com/example/desafio_sigabem/DesafioSigabemApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafio_sigabem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSigabemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSigabemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/desafio_sigabem/DesafioSigabemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_sigabem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSigabemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
